564753c73f72b8ac89f832aec560a809e4b415cc
yeetologist <71173561+yeetologist@users.noreply.github.com>
2023-05-14 14:47:05 +0700

    remove home dan data stats

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ app = MultiApp()
 
 # Add all your application here
 app.add_app("Input Data", input_data.app)
-app.add_app("Home", home.app)
-app.add_app("Data Stats", data_stats.app)
+# app.add_app("Home", home.app)
+# app.add_app("Data Stats", data_stats.app)
 
 # The main app
 app.run()
